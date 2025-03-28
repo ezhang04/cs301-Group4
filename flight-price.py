607adae7a94a87f9412712c1bdce3a65f6dfddfc
ezhang <ericzhang200@gmail.com>
2025-03-28 11:28:31 -0400

    read csv

--- flight-price.py
+++ flight-price.py
@@ -2,3 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+data = pd.read_csv("Clean_Dataset.csv")
+print(data.head())
