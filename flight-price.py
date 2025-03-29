3cac0673d826d87a843a0ec0ce38ba54c515fb24
ezhang <ericzhang200@gmail.com>
2025-03-28 21:43:04 -0400

    we are so goated

--- flight-price.py
+++ flight-price.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import root_mean_squared_error, mean_absolute_error
 from sklearn.metrics import r2_score
 
 data = pd.read_csv("Clean_Dataset.csv")
@@ -52,5 +52,5 @@ pipe.fit(X_train, y_train)
 y_test_pred = pipe.predict(X_test)
 
 print("Mean Absolute Error:", mean_absolute_error(y_test, y_test_pred))
-print("Root Mean Squared Error:", np.sqrt(mean_squared_error(y_test, y_test_pred)))
+print("Root Mean Squared Error:", root_mean_squared_error(y_test, y_test_pred))
 print("R2 Score: ", r2_score(y_test, y_test_pred))
